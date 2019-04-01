0a43cd7bd22e8359b0fc6aadda105ad4c5b1ddcf
Qun Li Huang <qunli02@gmail.com>
2019-04-01 18:20:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
   if array.length == 1
     array.join
+  elsif array.length == 2
+    array.insert[-1, "and "]
+    array.join[" "]
   else
-   array.insert[-1, "and"]
+   array.insert[-1, "and "]
    array.join(", ")
  end
 end
