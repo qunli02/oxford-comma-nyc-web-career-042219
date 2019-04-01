8243f49443d1dfd671d60995a882259437322dc4
Qun Li Huang <qunli02@gmail.com>
2019-04-01 18:24:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2
-    new = array.join[" and "]
+    new = array.join["and"]
     return new
   else
    array.insert[-1, "and "]
